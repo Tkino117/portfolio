0533d7160fa863f810ec67c73fd5a0aff608d091
Tkino117 <kinoshi117@gmail.com>
2026-04-19 02:05:21 +0900

    create about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import { About } from "@/components/about";
 import { HeroSection } from "@/components/hero-section";
 import { Introduction } from "@/components/introduction";
-import { SectionTitle } from "@/components/section-title";
 
 export default function Home() {
 	return (
 		<div>
 			<HeroSection />
 			<Introduction />
-			<SectionTitle>About</SectionTitle>
-			<Card>
-				<CardHeader>
-					<CardTitle>Hello World</CardTitle>
-				</CardHeader>
-				<CardContent>
-					<p>Hello World</p>
-				</CardContent>
-				<CardFooter>
-					<p>Hello World</p>
-				</CardFooter>
-			</Card>
+			<About />
 		</div>
 	);
 }
